7ac315f870fd243dfde379e8d2de9af0a58f933d
yoonsungkim87 <yoonsung.kim87@gmail.com>
2017-12-08 10:49:30 +0900

    'adj'

--- flask/app.py
+++ flask/app.py
@@ -13,13 +13,13 @@ def chart():
     server = win32.Dispatch('PISDK.PISDK.1').Servers('POSCOPOWER')
     pisdk = win32.gencache.EnsureModule('{0EE075CE-8C31-11D1-BD73-0060B0290178}',0, 1, 1,bForDemand = False)
     
-    tag, point = [None] * 2, [None] * 2
-    tag[0] = '70MKG31CQ001A_XQ01'
-    tag[1] = '80MKG31CQ001A_XQ01'
-    point[0] = server.PIPoints(tag[0]).Data
-    point[1] = server.PIPoints(tag[1]).Data
+    point = []
+    tag = ['70MKG31CQ001A_XQ01','80MKG31CQ001A_XQ01']
+    for x in tag:
+        point.append(server.PIPoints(x).Data)
     trends = []
     n_samples = 1000
+    
     for p in point:
         data2 = pisdk.IPIData2(p)
         results = data2.InterpolatedValues2('*-'+str(n_samples)+'h','*','1h',asynchStatus=None)
@@ -37,6 +37,7 @@ def chart():
         tmpValue.pop()
         tmpTime.pop()
         trends.append([tmpTime, tmpValue])
+        
     labels = trends[0][0]
     value1 = trends[0][1]
     value2 = trends[1][1]
